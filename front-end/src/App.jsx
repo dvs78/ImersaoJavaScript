5902dc09f138a3bd08f383381e69a4a6babf38b2
Daniel Veiga <dvs.veiga78@gmail.com>
2025-08-06 16:49:15 -0300

    Fazendo o 1 commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Home from "./pages/Home";
+// import Product from "./pages/Product";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Carrinho from "./components/Carrinho";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
