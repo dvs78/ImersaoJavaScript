fb551d43aa568dd9e67527438231ae5fab3a928a
Daniel Veiga <dvs.veiga78@gmail.com>
2025-08-14 22:46:03 -0300

    Criando post

--- back-end/index.js
+++ back-end/index.js
@@ -3,5 +3,19 @@ import express from "express";
 // Colocar todas as funções do express na variável app
 const app = express();
 
+// Criando requisição get = endpoint com barra, só tem resposta
+app.get("/", (req, res) => {
+  res.send("Olá, Galo!");
+});
+
+// Criando requisição get = endpoint com barra, tem requisição (body) e resposta (params)
+app.post("/", (req, res) => {
+  const body = req.body;
+  console.log(body.nome);
+  res.send("Informação nova criada com sucesso!");
+});
+
 // Colocar o app para rodar, ou seja, receber pedidos ou enviar respostas
-app.listen();
+app.listen(3000, () =>
+  console.log("Meu servidor está rodando na porta http://localhost:3000")
+);
